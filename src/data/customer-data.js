144ef2032e2b7fda6b10d6cfd0540fd2c0a9490c
harshita g <maxij143@gmail.com>
2020-07-19 16:32:39 -0500

    updated cutomer data

--- src/data/customer-data.js
+++ src/data/customer-data.js
@@ -1,10 +1,10 @@
 export default [
   {
-    title: 'I love my healthy driveway!',
+    title: 'My driveway is spotless and clean',
     content:
-      'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
-    customerName: 'Jane Doe',
-    customerTitle: 'Director of Research and Data',
+      'Me and my family are trying to put our house on the market, so we needed our house to look as polished as possible. Healthy driveway workers pressure washed our house to perfection. Our driveway used to have skid marks and oil spots, but now, it is cleaner than ever!',
+    customerName: 'Aruna Ganipineni',
+    customerTitle: 'Happy Healthy Driveway Customer',
     customerImage: 'https://placeimg.com/150/150/people'
   },
   {
